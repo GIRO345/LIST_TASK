b657077c97e9d9145fba58e0eff2f09c7dbe4546
Gironza <gironza343@gmail.com>
2025-05-05 20:27:51 -0500

    interfaz

---
+++ Proyecto-1/view.py
@@ -0,0 +1,8 @@
+def mostrar_menu():
+    print("\n~~~===Gestor de tareas===~~~")
+    print("1. Agregar tarea")
+    print("2. listar tarea")
+    print("3. Marcar tarea completada")
+    print("4. Eliminar tarea")
+    print("0. Salir")
+print (mostrar_menu())
